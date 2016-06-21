c34bc4650823a3f641f05b13b37e2f6945dc106e
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-21 20:23:09 +0300

    Added Basic Animation

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-var app = angular.module("mirror-ui", ['ui.bootstrap','ngRoute']);
+var app = angular.module("mirror-ui", ['ui.bootstrap','ngRoute','ngAnimate']);
 
 app.config(function ($routeProvider) {
     $routeProvider
