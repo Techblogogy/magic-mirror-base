848f37f0ef4d6997dda975d6c538a420276cea0d
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-21 18:29:24 +0300

    Added clock ticking

--- app/controllers/ClockController.js
+++ app/controllers/ClockController.js
@@ -1,5 +1,15 @@
-app.controller('ClockCtr', ['$scope', function ($scope) {
+app.controller('ClockCtr', ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.name = "Clock";
     $scope.show_title = false;
-    
+
+    $scope.time = Date.now();
+    $scope.tickInterval = 1000;
+
+    var tick = function () {
+        $scope.time = Date.now();
+        $timeout(tick, $scope.tickInterval);
+    }
+
+    $timeout(tick, $scope.tickInterval);
+
 }]);
