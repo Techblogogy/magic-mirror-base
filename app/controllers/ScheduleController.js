212b3112d5a3bd0b753cd02dd91579d6a8e0fa8a
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-21 15:32:37 +0300

    Adjusted demo clock widget. Made slimmer fonts

--- app/controllers/ScheduleController.js
+++ app/controllers/ScheduleController.js
@@ -1,9 +1,22 @@
 // Schedule Data Controller
 app.controller('ScheduleCtr', ['$scope', function ($scope) {
 
-    $scope.name = "Plans";
+    $scope.name = "Todays Plans";
 
-    $scope.plans = {
+    $scope.plans = [
+        {
+            time: "12:00pm - 14:00pm",
+            text: "Do some stuff"
+        },
 
-    };
+        {
+            time: "15:00pm - 15:30pm",
+            text: "Coffe with Dave"
+        },
+
+        {
+            time: "16:00pm - 16:05pm",
+            text: "Go have fun and this: antidisestablishmentarianism"
+        }
+    ];
 }]);
