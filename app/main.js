06c3f700db321133cbfe20f0a3aa6565110343c8
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-21 19:03:48 +0300

    Added base routing setup

--- app/main.js
+++ app/main.js
@@ -1,1 +1,9 @@
-var app = angular.module("mirror-ui", ['ui.bootstrap']);
+var app = angular.module("mirror-ui", ['ui.bootstrap','ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        controller: 'MainController',
+        templateUrl: 'app/views/home.html'
+    });
+});
