3b98a460291e135661854f9561b1fb44e0d40c16
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-21 13:40:15 +0300

    Adjusted window options

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
-    mainWindow = new BrowserWindow({width: 500, height: 899});
+    mainWindow = new BrowserWindow({width: 582, height: 1035, frame: false, resizable: true});
     mainWindow.loadURL('file://'+__dirname+'/index.html');
 
     mainWindow.on('closed', function (){
