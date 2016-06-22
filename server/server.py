1eef78856c1c7ef83373822162a9d091b8b9c6f8
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-22 21:06:02 +0300

    Test voice commands thread messaging

--- server/server.py
+++ server/server.py
@@ -5,18 +5,37 @@ from threading import Thread
 import time
 import speech_recognition as sr
 
-# Flask
+# Flask Elements
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret!";
 socketio = SocketIO(app,async_mode='threading')
 
+# Some junk
 client_id = ""
 thread = None
 
+# Voice Recognizer Elements
+r = sr.Recognizer()
+m = sr.Microphone()
+
+# Message ticking thread
 def tick_message():
     while True:
         time.sleep(10)
-        socketio.emit('myresponse', {'data': 'test'}, namespace='/io')
+        socketio.emit('myresponse', "GHost", namespace='/io')
+
+# Voice recognition callback
+def callback(recon, audio):
+    try:
+        text = recon.recognize_google(audio);
+        print("Google Speech: "+text)
+
+        socketio.emit("myresponse", text, namespace='/io')
+    except sr.UnknownValueError:
+        print("Google Speech unrecognizable")
+    except sr.RequestError as e:
+        print("Service unavalible")
+
 
 @app.route('/')
 def index():
@@ -27,35 +46,17 @@ def connected():
     global thread
 
     print "client connected"
-    client_id = request.sid
-    print(client_id)
+    print(request.sid)
+    # client_id = request.sid
 
-    if thread is None:
-        thread = Thread(target=tick_message)
-        thread.deamon = True
-        thread.start()
+    # if thread is None:
+    #     thread = Thread(target=tick_message)
+    #     thread.deamon = True
+    #     thread.start()
 
-
-# @socketio.on('myevent')
-# def test_message(message):
-    # print(message)
-
-# r = sr.Recognizer()
-# m = sr.Microphone()
-#
-# def callback(recon, audio):
-#     try:
-#         text = recon.recognize_google(audio);
-#         print("Google Speech: "+text)
-#         socketio.emit("myresponse", 'swing', namespace='/', room=client_id)
-#     except sr.UnknownValueError:
-#         print("Google Speech unrecognizable")
-#     except sr.RequestError as e:
-#         print("Service unavalible")
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+    stop_listening = r.listen_in_background(m,callback)
 
 if __name__  == '__main__':
-    # with m as source:
-    #     r.adjust_for_ambient_noise(source)
-    # stop_listening = r.listen_in_background(m,callback)
-
     socketio.run(app, debug=True)
