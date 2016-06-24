9a9713c105c4ae37d72ed9da100c3c2e940de1a0
Fedor Bobylev <fedorbobylev@gmail.com>
2016-06-24 14:43:10 +0300

    Fixed database boilerplate data fetching

--- server/dbase/dbase.py
+++ server/dbase/dbase.py
@@ -2,11 +2,10 @@ import sqlite3
 
 class dbase:
     def __init__(self):
-        self._dbpath = 'res/db/mirror.db' # Database file path
+        print ("create")
 
-        self.connect()
-        # self._cn = sqlite3.connect(self._dbpath) # Created Database "connection"
-        # self._db = self._cn.cursor() # Databse Cursor
+        self._dbpath = 'res/db/mirror.db' # Database file path
+        self.setup()
 
     # Connect to database
     def connect(self):
@@ -16,16 +15,34 @@ class dbase:
     # Querry Database
     def qry(self, qry):
         self.connect()
-        self._db.execute(qry)
+        dat = self.exe(qry)
         self.close()
+        return dat
 
     # Only execute querry
     def exe(self, qry):
         self._db.execute(qry)
+        return self._db.fetchall()
 
     # Commit changes and close
     def close(self):
         self._cn.commit()
         self._cn.close()
 
+    #TODO: Add Setup method to initate all of the tables
+    def setup(self):
+        self.connect()
+
+        # Create a basic weather test table
+        self.exe("""
+            CREATE TABLE IF NOT EXISTS test (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                temperature INTEGER NOT NULL,
+                icon TEXT NOT NULL
+            )
+            """)
+
+        self.close()
+
+# Initiate database instance
 db = dbase()
--- server/server.py
+++ server/server.py
@@ -7,24 +7,27 @@ from dbase.dbase import db
 import time
 import speech_recognition as sr
 
+IO_SPACE = "/io"
+
 # Flask Elements
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "supersecret";
 socketio = SocketIO(app,async_mode='threading')
 
 # Some junk
-client_id = ""
+# client_id = ""
 thread = None
 
 # Voice Recognizer Elements
-r = sr.Recognizer()
-m = sr.Microphone()
+
+# r = sr.Recognizer()
+# m = sr.Microphone()
 
 # Message ticking thread
 def tick_message():
     while True:
         time.sleep(10)
-        socketio.emit('myresponse', "GHost", namespace='/io')
+        socketio.emit('myresponse', "GHost", namespace=IO_SPACE)
 
 # Voice recognition callback
 def callback(recon, audio):
@@ -32,7 +35,7 @@ def callback(recon, audio):
         text = recon.recognize_google(audio);
         print("Google Speech: "+text)
 
-        socketio.emit("myresponse", text, namespace='/io')
+        socketio.emit("myresponse", text, namespace=IO_SPACE)
     except sr.UnknownValueError:
         print("Google Speech unrecognizable")
     except sr.RequestError as e:
@@ -41,24 +44,24 @@ def callback(recon, audio):
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "Welcome to Magic Mirror Server :)"
 
 @socketio.on("connect", namespace='/io')
 def connected():
     global thread,db
 
-    print "client connected"
-    print(request.sid)
-    # client_id = request.sid
+    print "client %s connected" % (request.sid)
+
+    emit("myresponse", db.qry("SELECT * FROM test"))
 
     # if thread is None:
     #     thread = Thread(target=tick_message)
     #     thread.deamon = True
     #     thread.start()
 
-    with m as source:
-        r.adjust_for_ambient_noise(source)
-    stop_listening = r.listen_in_background(m,callback)
+    # with m as source:
+    #     r.adjust_for_ambient_noise(source)
+    # stop_listening = r.listen_in_background(m,callback)
 
 if __name__  == '__main__':
     socketio.run(app, debug=True)
