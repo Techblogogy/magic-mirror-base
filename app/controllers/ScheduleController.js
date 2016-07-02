c58fc0c0a51e6a833fc2e2333eccce59da59ce85
tbatsenko <tbatsenko@gmail.com>
2016-07-02 16:12:14 +0300

    added Plans for Today

--- app/controllers/ScheduleController.js
+++ app/controllers/ScheduleController.js
@@ -1,7 +1,7 @@
 // Schedule Data Controller
 app.controller('ScheduleCtr', ['$scope', '$http',function ($scope, $http) {
 
-    $scope.name = "Todays Plans";
+    $scope.name = "Plans for Today:";
     $scope.show_title = true;
 
     $scope.loaded = function () {
@@ -10,28 +10,29 @@ app.controller('ScheduleCtr', ['$scope', '$http',function ($scope, $http) {
 
 
     $http.get('http://localhost:5000/gcal/today')
-    .success(function(a){
-        console.log("111");
-        console.log(a);
+    .success(function(data){
+        console.log(data);
+        $scope.plans = data;
+
     })
 
-    $scope.plans = [
-        {
-            time_start: "12:00pm",
-            time_end: "14:00pm",
-            text: "Do some stuff"
-        },
-
-        {
-            time_start: "15:00pm",
-            time_end: "15:30pm",
-            text: "Coffe with Dave"
-        },
-
-        {
-            time_start: "16:00pm",
-            time_end: "16:05pm",
-            text: "Go have fun and this: antidisestablishmentarianism"
-        }
-    ];
+    // $scope.plans = [
+    //     {
+    //         time_start: "12:00pm",
+    //         time_end: "14:00pm",
+    //         text: "Do some stuff"
+    //     },
+    //
+    //     {
+    //         time_start: "15:00pm",
+    //         time_end: "15:30pm",
+    //         text: "Coffe with Dave"
+    //     },
+    //
+    //     {
+    //         time_start: "16:00pm",
+    //         time_end: "16:05pm",
+    //         text: "Go have fun and this: antidisestablishmentarianism"
+    //     }
+    // ];
 }]);
