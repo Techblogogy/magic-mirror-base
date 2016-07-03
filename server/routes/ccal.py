060e4342584d1de05131f81a12c8e28655055cfa
Fedor Bobylev <fedorbobylev@gmail.com>
2016-07-03 13:17:57 +0300

    fixed ccal Blueprint routes

--- server/routes/ccal.py
+++ server/routes/ccal.py
@@ -9,17 +9,17 @@ ccal_api = Blueprint('ccal_api', __name__, url_prefix="/cal")
 # Calendar API Routes
 
 # main route
-@ccal_api.route('/cal')
+@ccal_api.route('/')
 def cal_route():
     return "";
 
 # events getter
-@ccal_api.route('/cal/events/get', methods=['GET'])
+@ccal_api.route('/events/get', methods=['GET'])
 def cal_get_event():
     return json.dumps(cal.get_events(), indent=JSON_DENT)
 
 # events getter in range
-@ccal_api.route('/cal/events/get/range', methods=['GET'])
+@ccal_api.route('/events/get/range', methods=['GET'])
 def cal_get_rage_event():
     return json.dumps(cal.get_range_events(
         request.args.get('min'),
@@ -27,12 +27,12 @@ def cal_get_rage_event():
     ), indent=JSON_DENT)
 
 # today event getter
-@ccal_api.route('/cal/events/get/today', methods=['GET'])
+@ccal_api.route('/events/get/today', methods=['GET'])
 def cal_get_today_event():
     return json.dumps(cal.get_today_events(), indent=JSON_DENT)
 
 # events adder
-@ccal_api.route('/cal/events/add', methods=['POST'])
+@ccal_api.route('/events/add', methods=['POST'])
 def cal_add_event():
     return cal.add_event(
         request.form.get('task'),
@@ -40,7 +40,7 @@ def cal_add_event():
         request.form.get('time'))
 
 # events updater
-@ccal_api.route('/cal/events/update', methods=['POST'])
+@ccal_api.route('/events/update', methods=['POST'])
 def cal_upt_event():
     return cal.upd_event(
         request.form.get('id', type=int),
@@ -49,6 +49,6 @@ def cal_upt_event():
         request.form.get('time'))
 
 # events remover
-@ccal_api.route('/cal/events/delete', methods=['POST'])
+@ccal_api.route('/events/delete', methods=['POST'])
 def cal_rmv_event():
     return cal.rmv_event(request.form.get('id', type=int))
