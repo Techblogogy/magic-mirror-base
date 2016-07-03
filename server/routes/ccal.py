bb45422f654a2945cf0c4f31c507592415e169dd
Fedor Bobylev <fedorbobylev@gmail.com>
2016-07-03 13:15:08 +0300

    Fixed custom calendar api

--- server/routes/ccal.py
+++ server/routes/ccal.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, redirect
-from api_cal.calendar import cal
+api_cal.calendar import cal
 
 import os, json
 
@@ -16,12 +16,12 @@ def cal_route():
 # events getter
 @ccal_api.route('/cal/events/get', methods=['GET'])
 def cal_get_event():
-    return json.dumps(calendar.calendar.cal.get_events(), indent=JSON_DENT)
+    return json.dumps(cal.get_events(), indent=JSON_DENT)
 
 # events getter in range
 @ccal_api.route('/cal/events/get/range', methods=['GET'])
 def cal_get_rage_event():
-    return json.dumps(calendar.calendar.cal.get_range_events(
+    return json.dumps(cal.get_range_events(
         request.args.get('min'),
         request.args.get('max')
     ), indent=JSON_DENT)
@@ -29,12 +29,12 @@ def cal_get_rage_event():
 # today event getter
 @ccal_api.route('/cal/events/get/today', methods=['GET'])
 def cal_get_today_event():
-    return json.dumps(calendar.calendar.cal.get_today_events(), indent=JSON_DENT)
+    return json.dumps(cal.get_today_events(), indent=JSON_DENT)
 
 # events adder
 @ccal_api.route('/cal/events/add', methods=['POST'])
 def cal_add_event():
-    return calendar.calendar.cal.add_event(
+    return cal.add_event(
         request.form.get('task'),
         request.form.get('date'),
         request.form.get('time'))
@@ -42,7 +42,7 @@ def cal_add_event():
 # events updater
 @ccal_api.route('/cal/events/update', methods=['POST'])
 def cal_upt_event():
-    return calendar.calendar.cal.upd_event(
+    return cal.upd_event(
         request.form.get('id', type=int),
         request.form.get('task'),
         request.form.get('date'),
@@ -51,4 +51,4 @@ def cal_upt_event():
 # events remover
 @ccal_api.route('/cal/events/delete', methods=['POST'])
 def cal_rmv_event():
-    return calendar.calendar.cal.rmv_event(request.form.get('id', type=int))
+    return cal.rmv_event(request.form.get('id', type=int))
