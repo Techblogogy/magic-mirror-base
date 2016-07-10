eab918254d91a02df323b226624f4dca1ebf8644
Fedor Bobylev <fedorbobylev@gmail.com>
2016-07-10 16:25:15 +0300

    Added ranged wardrobe get function

--- server/cvison/store.py
+++ server/cvison/store.py
@@ -10,6 +10,7 @@ class clothes:
                 name TEXT,
                 thumbnail TEXT NOT NULL,
                 dresscode TEXT NOT NULL,
+                temperature TEXT NOT NULL,
                 tags TEXT,
                 deleted INTEGER NOT NULL DEFAULT 0
             )
@@ -23,10 +24,19 @@ class clothes:
             (name, thumbnail, dresscode, tags, )
         )
 
+    # Get all items
     @classmethod
     def get_all(self):
         return db.qry("SELECT name, thumbnail, dresscode, tags FROM clothes WHERE deleted=0")
 
+    # Get letters in range
+    @classmethod
+    def get(self, lim, ofs):
+        return db.qry(
+            "SELECT id, name, thumbnail, dresscode, tags FROM clothes WHERE deleted=0 LIMIT ? OFFSET ?",
+            (lim, ofs)
+        )
+
 # clothes.setup()
 # clothes.add("casual", "1.png", "somthing 1", "cool, winter")
-# print clothes.get_all()[0]['dresscode']
+# print clothes.get(3,3)
