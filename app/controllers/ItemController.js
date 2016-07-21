f9f2eaf401b892ad771c6c26c19c605969352ae8
tbatsenko <tbatsenko@gmail.com>
2016-07-21 13:26:42 +0300

    added wardrobe switch

--- app/controllers/ItemController.js
+++ app/controllers/ItemController.js
@@ -1,16 +1,33 @@
-app.controller('ItemCtr', ['$scope', function($scope) {
+app.controller('ItemCtr', ['$scope', '$document','$http', function($scope, $document, $http) {
+    $scope.loaded = function(){};
     $scope.items =[
       {
-        icon: "res/pics/cat.jpg",
-        title: 'MOVE',
+        thumbnail: "res/pics/cat.jpg",
+        f: 'MOVE',
         developer: 'MOVE, Inc.',
         price: 0.99
-      },
-      {
-        icon: 'img/shutterbugg.jpg',
-        title: 'Shutterbugg',
-        developer: 'Chico Dusty',
-        price: 2.99
       }
-    ]
+  ];
+    $scope.x = 2;
+    $scope.add_item = function(){
+        $scope.clth = [];
+        $http.post('http://localhost:5000/wardrobe/add',$scope.clth)
+        .success(function(d){
+            $scope.items = d;
+            console.log(d);
+        });
+    };
+
+    // $scope.add_item();
+
+    $http.get('http://localhost:5000/wardrobe/get/all?items='+16+'&page='+0)
+    .success(function(data){
+        console.log(data);
+        $scope.items = data;
+        setTimeout(function () {
+            console.log($document.find("#item-1"));
+            $document.find("#item-1").addClass("current");
+            angular.element(document.querySelectorAll("#item-1")).addClass("current");
+        }, 1000);
+    });
 }]);
--- app/controllers/StylistPageController.js
+++ app/controllers/StylistPageController.js
@@ -1,7 +1,25 @@
-app.controller('StlCtr', ['$scope', function ($scope) {
-
+app.controller('StlCtr', ['$scope','$document', function ($scope,$document) {
+    $scope.loaded = function(){};
     $scope.page_id = "p_stylist";
     $scope.img = {
         cat: "res/pics/cat.jpg"
-    }
+    };
+    var x = 1;
+    $scope.switch_right = function(){
+        // $document.find("current").removeClass("current");
+        angular.element(document.querySelectorAll("#item-"+x)).removeClass("current");
+        x += 1;
+        angular.element(document.querySelectorAll("#item-"+x)).addClass("current");
+
+        // angular.element(document.querySelectorAll("#item-{{x}}")).addClass("current");
+    };
+    $scope.switch_left = function(){
+        // $document.find("current").removeClass("current");
+        angular.element(document.querySelectorAll("#item-"+x)).removeClass("current");
+        x -= 1
+        angular.element(document.querySelectorAll("#item-"+x)).addClass("current");
+
+        // angular.element(document.querySelectorAll("#item-{{x}}")).addClass("current");
+    };
+
 }]);
