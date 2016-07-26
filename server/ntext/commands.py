276f37f4e1b540659284fc87f035e4007375565f
tbatsenko <tbatsenko@gmail.com>
2016-07-26 18:10:16 +0300

    Show/close image

--- server/ntext/commands.py
+++ server/ntext/commands.py
@@ -11,7 +11,8 @@ in_cmd = [
     v_cmd(["go","right"], "AND", "right"),
     v_cmd(["go","left"], "AND", "left"),
     v_cmd([], "NUM", "fullscreen"),
-    v_cmd(["close", "image"], "AND", "fullscreen"),
+    v_cmd(["close", "image"], "AND", "close_item")
+    # v_cmd([], "TAG", "tag"),
 
 
 
