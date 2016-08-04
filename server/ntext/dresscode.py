743b8025b181bb5458106170eb352fa0f3cbe4a6
Fedor Bobylev <fedorbobylev@gmail.com>
2016-08-04 07:47:58 +0300

    Dresscode recognition base

---
+++ server/ntext/dresscode.py
@@ -0,0 +1,13 @@
+# import nltk
+
+dtags = {
+    'business-casual': [""],
+    'sportswear': ["run", "jog", "yoga"]
+    'formal': [""],
+    'casual': [""],
+}
+
+def get_dresscode(cal):
+    pass
+
+# import sklearn
