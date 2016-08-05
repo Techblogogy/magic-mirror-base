7c85c715135cb44ce8e1d680d4d259098c236f1f
tbatsenko <tbatsenko@gmail.com>
2016-08-05 17:37:21 +0300

    Switching pages on setupSite

--- server/routes/WDmanager.py
+++ server/routes/WDmanager.py
@@ -1,3 +1,4 @@
+
 import decor
 
 from flask import Blueprint, redirect, request, url_for, render_template
@@ -12,15 +13,24 @@ JSON_DENT = 4
 wd_manager_api = Blueprint('wd_manager_api', __name__, url_prefix="/manager")
 
 # Returns Wardrobe items page
-
+page_counter = 0
 @wd_manager_api.route('/')
 def wardrobe_manager():
+    global page_counter
     print request.args.get("id")
 
     if request.args.get('action') == "delete":
         clothes.delete(request.args.get("id"))
+    if request.args.get('action') == "nextpage":
+        if page_counter < clothes.page_count(24) - 1 :
+           page_counter += 1
+           print page_counter
+    if request.args.get('action') == "prevpage":
+        if page_counter > 0:
+           page_counter -= 1
 
-    return (render_template('wardrobe.html', items = clothes.get(24,0)))
+
+    return (render_template('wardrobe.html', items = clothes.get(24,page_counter), page_counter = page_counter))
 #
 # @wd_manager_api.route('/')
 # def smthr():
