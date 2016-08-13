103c932051448de9272d86c972056702dba76063
Fedor Bobylev <fedorbobylev@gmail.com>
2016-08-13 17:42:21 +0300

    Updated ip address in test script

--- server/upload.py
+++ server/upload.py
@@ -1,7 +1,7 @@
 import requests, json
 
 def upload():
-    url = "http://52.174.96.93/"
+    url = "http://13.95.237.230:8000/"
     file = {'file': open('static/bg.jpg', 'rb')}
 
     r = requests.post(url, files=file)
