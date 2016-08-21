7c622937fc9fbef7cef9589cf4e63897afe83200
Fedor Bobylev <fedorbobylev@gmail.com>
2016-08-21 21:23:45 +0300

    added config file parser file

---
+++ server/tb_config.py
@@ -0,0 +1,16 @@
+import ConfigParser
+
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class conf_file():
+    __metaclass__ = Singleton
+
+    def __init__():
+        self.config = ConfigParser.ConfigParser()
+        self.config.read('config.cfg')
