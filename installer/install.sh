6b8f7432bcb036bf3b305f6227a9aded188e85ae
Fedor Bobylev <fedorbobylev@gmail.com>
2016-10-27 19:53:00 +0300

    Added basic install script

---
+++ installer/install.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# This is a basic Magic Mirror installation image
+
+# NOTE: Make sure you install this on clean Raspbian Jessie image
+# for maximum compatability
+
+# === System (Part 1)
+
+# Apt-Update
+echo -e "\e[35mUpdating linux repositories\e[0m"
+sudo apt-get update
+
+
+# === Repository Clone
+cd ~
+
+# Getting Magic Mirror Repository
+echo -e "\e[35mDownloading Magic Mirror repository\e[0m"
+if https://github.com/Techblogogy/magic-mirror-base.git; then
+    echo -e "\e[32mRepository downloaded\e[0m"
+else
+    echo -e "\e[31mRepository download failed\e[0m"
+    exit;
+fi
+
+cd ~/magic-mirror-base || exit
+
+
+# === NodeJS
+
+# Install NodeJS
+echo -e "\e[35mInstalling NodeJS\e[0m"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs npm
+echo -e "\e[32mNodeJS Installed\e[0m"
+
+# Npm install
+echo -e "\e[35mInstalling javascript packages\e[0m"
+if npm install; then
+    echo -e "\e[35mJS Packages installed\e[0m"
+else
+    echo -e "\e[31mJS Packages install failed"
+    exit;
+fi
+
+
+# === Python
+
+cd ~/magic-mirror-base/server || exit
+
+# Install Python Dependencies
+echo -e "\e[35mInstalling Python-dev\e[0m"
+if sudo apt-get install -y python-dev; then
+    echo -e "\e[32mPython Dev installed\e[0m"
+else
+    echo -e "\e[31mPython Dev install failed"
+    exit;
+fi
+
+# Install pip
+echo -e "\e[35mInstalling pip\e[0m"
+if sudo apt-get install -y python-pip; then
+    echo -e "\e[32mPip installed\e[0m"
+else
+    echo -e "\e[31mPip install failed"
+    exit;
+fi
+
+# Install Pyaudio
+echo -e "\e[35mInstalling Pyaudio\e[0m"
+if sudo apt-get install -y python-pyaudio; then
+    echo -e "\e[32mPyaudio installed\e[0m"
+else
+    echo -e "\e[31mPyaudio install failed"
+    exit;
+fi
+
+# Install Pip Dependencies
+echo "\e[35mInstalling pip dependencies. This may take a while, please wait...\e[0m"
+if sudo pip install -r requirments.txt; then
+    echo -e "\e[32mPip dependencies installed\e[0m"
+else
+    echo -e "\e[31mPip dependencies install failed"
+    exit;
+fi
